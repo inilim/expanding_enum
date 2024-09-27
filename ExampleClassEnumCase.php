9a982af062060f3351d22f108bc72dc3563ca46a
Dmitry <40324321+inilim@users.noreply.github.com>
2024-09-27 23:36:37 +0300

    example

--- ExampleClassEnumCase.php
+++ ExampleClassEnumCase.php
@@ -8,4 +8,14 @@ use Inilim\ExpandingEnum\EnumCase;
 /**
  * @template-extends EnumCase<ExampleEnum>
  */
-class ExampleClassEnumCase extends EnumCase {}
+class ExampleClassEnumCase extends EnumCase
+{
+    function getHeader(): string
+    {
+        return match ($e = $this->e()) {
+            $e::ONE   => 'Один',
+            $e::TWO   => 'Два',
+            $e::THREE => 'Три',
+        };
+    }
+}
